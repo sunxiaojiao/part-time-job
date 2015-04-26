6b589da2b109daf2a349a16a2ebe28734c558450
airect <sunxiaojiao001@gmail.com>
2015-04-26 23:01:03 +0800

    注册-更改邮箱验证码为验证链接

--- Lib/Action/RegisterAction.class.php
+++ Lib/Action/RegisterAction.class.php
@@ -58,20 +58,27 @@ class RegisterAction extends Action{
 			return $flag;
 		}
 	}
-	
+	protected function buildUrl($email) {
+		$rand = ranVerify(8);
+		C('URL_MODEL', 0);
+		$url = U("Register/confirm","email=$email&code=$rand",'',false,true);
+		$MailReg = M('Mailreg_url');
+		$data = array('email'=>$email,'vld_code_value'=>$rand);
+		$MailReg->add($data);
+		return $url;
+	}
 	/**
 	 * 发送验证邮件
 	 * @param string $to 收信人
 	 * 0:邮件发送失败 1:邮件发送成功
 	 */
 	public function sendEmail(){
-		$verify = ranVerify();
 		$this->email = $this->_post('email');
-		
+		$vld_url = $this->buildUrl($this->email);
 		$title = "小蜜蜂兼职";
 		$message = <<<EOT
 		您的邮箱为：{$this->email},\n
-		您的验证码为：{$verify}
+		您的验证码为：{$vld_url}
 EOT;
 		if(SendMail($this->email, $title, $message)){
 			//设置session
@@ -84,13 +91,35 @@ EOT;
 
 	}
 	//判断邮箱验证码
+//	public function confirm(){
+//		if(session('email_ver') == $this->_post('yzm')){
+//			echo 1;
+//			return true;
+//		}else{
+//			echo 0;
+//			return false;
+//		}
+//	}
 	public function confirm(){
-		if(session('email_ver') == $this->_post('yzm')){
-			echo 1;
-			return true;
+		$email    = $this->_get('email');
+		$vld_code = $this->_get('code');
+		if(empty($email) || empty($vld_code)){
+			$this->ajaxReturn(0,"验证失败",0);
+			return;
+		}
+		$reg = M('Mailreg_url');
+		$reg_id = $reg->field("reg_id")
+				   	  ->where("email=" . "'" . $email . "'" . " AND " . "vld_code_value=" . "'" . "$vld_code" . "'")
+				   	  ->find();
+		if($reg_id){
+			session('reg_vld',1);
+			$MailReg = M('Mailreg_url');
+			$MailReg->where("email=".$email)->setField("ispass",1);
+			$this->success("邮箱验证成功","/",3);
 		}else{
-			echo 0;
-			return false;
+			dump($reg->getLastSql());
+			$this->error('验证链接错误',"http://www.xiaomifengjob.com",3);
+			return ;
 		}
 	}
 	private function getAll(){
