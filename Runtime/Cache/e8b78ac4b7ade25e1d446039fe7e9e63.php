baa13f81eefb6d93f2feff2bbfb0074f89e72ae4
airect <sunxiaojiao001@gmail.com>
2015-06-06 01:18:20 +0800

    修改favicon.ico

--- Runtime/Cache/e8b78ac4b7ade25e1d446039fe7e9e63.php
+++ Runtime/Cache/e8b78ac4b7ade25e1d446039fe7e9e63.php
@@ -5,8 +5,8 @@
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
     <title>兼职平台</title>
-    <link href="/Public/favicon.ico" type="image/x-icon" rel=icon />
-<link href="/Public/favicon.ico" type="image/x-icon" rel="shortcut icon" />
+    <link href="/Public/xmf32.ico" type="image/x-icon" rel=icon />
+<link href="/Public/xmf32.ico" type="image/x-icon" rel="shortcut icon" />
 <link rel="stylesheet" href="/__GROUP__/css/bootstrap.min.css" />
 <link rel="stylesheet" href="/__GROUP__/css/bootstrap-theme.min.css" />
 <link rel="stylesheet" href="/__GROUP__/css/common.css">
