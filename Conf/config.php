bdddad0b63dcfbf9874cd33151ee8b46ef1cc8b2
airect <sunxiaojiao001@gmail.com>
2015-07-02 18:41:15 +0800

    哦

--- Conf/config.php
+++ Conf/config.php
@@ -13,8 +13,8 @@ return array(
         '__GROUP__' => 'Tpl/bootstrap',
     ),
     
-    'TOKEN_ON'=>true,  // 是否开启令牌验证
-	'TOKEN_NAME'=>'__hash__',    // 令牌验证的表单隐藏字段名称
+    'TOKEN_ON'=>false,  // 是否开启令牌验证
+	'TOKEN_NAME'=>'_hash_',    // 令牌验证的表单隐藏字段名称
 	'TOKEN_TYPE'=>'md5',  //令牌哈希验证规则 默认为MD5
 	'TOKEN_RESET'=>true,  //令牌验证出错后是否重置令牌 默认为true
     
--- Runtime/Cache/a4701eeb53e71e57ef42f5ba34b93d8a.php
+++ Runtime/Cache/a4701eeb53e71e57ef42f5ba34b93d8a.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
-  <title><?php echo ($userinfo["username"]); ?>的简历-小蜜蜂兼职</title>
+  <title><?php echo ($userinfo["username"]); ?>的简历-小蜜蜂job</title>
 
 <link href="/Public/xmf32.ico" type="image/x-icon" rel=icon />
 <link href="/Public/xmf32.ico" type="image/x-icon" rel="shortcut icon" />
@@ -48,7 +48,7 @@
         <span class="icon-bar"></span>
         <span class="icon-bar"></span>
       </button>
-      <a class="" href="/"><img src="/Public/logo/logo.png" height="50" alt="小蜜蜂兼职logo" /></a>
+      <a class="" href="/"><img src="/Public/logo/logo.jpg" height="50" alt="小蜜蜂兼职logo" /></a>
     </div>
 
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
@@ -239,9 +239,9 @@ THINK;
 <!--footer-->
 <div class="container">
   <hr />
-  <p class="text-center">小蜜蜂兼职</p>
-  <p class="my-info text-center"><a href="http://www.xiaomifengjob.com">首页</a>/<a href="<?php echo U("Advice/index");?>">投诉建议</a>/<a href="http://www.xiaomifengjob.com">关于小蜜蜂</a>/<a href="http://www.xiaomifengjob.com">联系我们</a></p>
-  <p class="copyright text-center">Copyright ©小蜜蜂网络 / 备案号：ICP备13008243号-1 / 地址：烟台市红旗中路</p>
+  <p class="text-center">梦海网络</p>
+  <p class="my-info text-center"><a href="http://www.xiaomifengjob.com">首页</a>/<a href="<?php echo U("Advice/index");?>">投诉建议</a>/<a href="http://www.xiaomifengjob.com">关于梦海网络</a>/<a href="http://www.xiaomifengjob.com">联系我们</a></p>
+  <p class="copyright text-center">Copyright © 梦海网络 / 备案号：/ 地址：烟台市红旗中路</p>
   <p class="hidden"><script src="http://s11.cnzz.com/z_stat.php?id=1255390287&web_id=1255390287" language="JavaScript"></script></p>
 </div>
 <!--./footer-->
